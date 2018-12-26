62acf6728a59a60a17bcc77d45c368c0e938cfdc
starsky1 <763559353@qq.com>
2018-12-26 19:02:23 +0800

    first commit

---
+++ src/main/java/com/yangjing/corpwechattools/CorpWechatToolsApplication.java
@@ -0,0 +1,14 @@
+package com.yangjing.corpwechattools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpWechatToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpWechatToolsApplication.class, args);
+	}
+
+}
+
